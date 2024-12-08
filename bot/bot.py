8c3753093da05002b8527fd00b1dcd04b9e6b5a9
vasilisk7258 <ertybasevk@gmail.com>
2024-12-08 11:54:48 +0300

    0.0.006

--- bot/bot.py
+++ bot/bot.py
@@ -22,4 +22,6 @@ async def main() -> None:
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+    #print()
     asyncio.run(main())
+    
--- bot/callbacks/cross_zeroes_callBack.py
+++ bot/callbacks/cross_zeroes_callBack.py
@@ -1,8 +1,24 @@
 from aiogram import Router, F
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, InlineQuery
+from bot.keyboards.inline_keyboard import create_cross_aeroes
 
 router = Router()
 
-@router.callback_query(F.data.in_(str(range(9))))
+@router.callback_query(F.data.in_([str(i) for i in range(9)]))
 async def mark_button(query: CallbackQuery):
+    #print(query)
+    print("kjssgdddddddddddf")
+    #await query.message.answer("djkdshfhkd")
+    keyboard = create_cross_aeroes()
+    #await query.answer("соси",show_alert=True)
+    #print("asjgd")
+    #print(query.data)
+    #print(keyboard.inline_keyboard[0])
     print(query)
+    if(int(query.data) in range(3)):
+        keyboard.inline_keyboard[0][int(query.data)].text = "1"
+    if(int(query.data) in range(3,6)):
+        keyboard.inline_keyboard[1][int(query.data)-3].text = "1"
+    if(int(query.data) in range(6,9)):
+        keyboard.inline_keyboard[2][int(query.data)-6].text = "1"
+    await query.bot.edit_message_reply_markup(inline_message_id=query.inline_message_id, reply_markup=keyboard)

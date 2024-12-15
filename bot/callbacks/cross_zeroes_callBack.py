d3e6986acc1e71522e2f2c16307206697d8eb3da
vasilisk7258 <ertybasevk@gmail.com>
2024-12-15 22:05:50 +0300

    0.0.008

--- bot/callbacks/cross_zeroes_callBack.py
+++ bot/callbacks/cross_zeroes_callBack.py
@@ -11,30 +11,39 @@ async def mark_button(query: CallbackQuery):
     # добавление второго пользователя
     if properties["players"][1] == "" and properties["move"] == "" and properties["players"][0] != query.from_user.username:
         properties["players"][1] = query.from_user.username
+        if properties["first_player"] == "":
+            properties["first_player"] = query.from_user.username
         properties["move"] = query.from_user.username
     if properties["move"] == query.from_user.username:
         if properties["first_player"] == query.from_user.username:
             symbol = "X"
         else:
             symbol = "O"
-        if(int(query.data) in range(3)):
+        if int(query.data) in range(3) and properties["keyboard"].inline_keyboard[0][int(query.data)].text == " ":
             properties["keyboard"].inline_keyboard[0][int(query.data)].text = symbol
-        if(int(query.data) in range(3,6)):
+        elif(int(query.data) in range(3,6)and properties["keyboard"].inline_keyboard[1][int(query.data)-3].text == " "):
             properties["keyboard"].inline_keyboard[1][int(query.data)-3].text = symbol
-        if(int(query.data) in range(6,9)):
+        elif(int(query.data) in range(6,9) and properties["keyboard"].inline_keyboard[2][int(query.data)-6].text == " "):
             properties["keyboard"].inline_keyboard[2][int(query.data)-6].text = symbol
-        properties["move"] = properties["players"][1 - properties["players"].index(query.from_user.username)]
-        if properties["move"] == properties["players"][0]:
-            if properties["first_player"] == properties["players"][0]:
-                text = (f"игра в крестики-нолики\n\n --> @{properties["players"][0]} X \n @{properties["players"][1]} O")
-            else:
-                text = (f"игра в крестики-нолики\n\n --> @{properties["players"][0]} O \n @{properties["players"][1]} X")
         else:
-            if properties["first_player"] == properties["players"][0]:
-                text = (f"игра в крестики-нолики\n\n@{properties["players"][0]} X \n--> @{properties["players"][1]} O")
+            await query.answer("Выбери свободную клетку")
+            return
+        if await game.crossZeroes.check_win(query.inline_message_id):
+            text = f"Победил @{query.from_user.username}"
+            await query.bot.edit_message_text(text=text, inline_message_id=query.inline_message_id)
+        else:
+            properties["move"] = properties["players"][1 - properties["players"].index(query.from_user.username)]
+            if properties["move"] == properties["players"][0]:
+                if properties["first_player"] == properties["players"][0]:
+                    text = (f"игра в крестики-нолики\n\n --> @{properties["players"][0]} X \n @{properties["players"][1]} O")
+                else:
+                    text = (f"игра в крестики-нолики\n\n --> @{properties["players"][0]} O \n @{properties["players"][1]} X")
             else:
-                text = (f"игра в крестики-нолики\n\n@{properties["players"][0]} O \n--> @{properties["players"][1]} X")
-        await query.bot.edit_message_text(inline_message_id=query.inline_message_id,
-                                          text=text, reply_markup=properties["keyboard"])
+                if properties["first_player"] == properties["players"][0]:
+                    text = (f"игра в крестики-нолики\n\n@{properties["players"][0]} X \n--> @{properties["players"][1]} O")
+                else:
+                    text = (f"игра в крестики-нолики\n\n@{properties["players"][0]} O \n--> @{properties["players"][1]} X")
+            await query.bot.edit_message_text(inline_message_id=query.inline_message_id,
+                                            text=text, reply_markup=properties["keyboard"])
     else:
         await query.answer("Не твой ход", show_alert=True)
--- gameControll/crossZeroes.py
+++ gameControll/crossZeroes.py
@@ -5,7 +5,9 @@ class CrossZeroes:
     #room_id: int = 0
     symbols = "XO"
     private_rooms: dict = {}
-    async def create__private_room(self, username: str, keyboard: InlineKeyboardMarkup, inline:str) -> None:
+    async def create__private_room(self, username: str, 
+                                   keyboard: InlineKeyboardMarkup, 
+                                   inline:str) -> None:
         players = [username, ""]
         first = random.choice(players)
         self.private_rooms.update({inline: {"players": [username, ""],
@@ -13,5 +15,33 @@ class CrossZeroes:
                                             "move": first, 
                                             "keyboard": keyboard
                                             }})
-        
+    async def check_win(self, id:str) -> bool:
+        field = self.private_rooms[id]["keyboard"].inline_keyboard
+        for i in range(0,8):
+            match i:
+                case 0:
+                    l = field[0][0].text + field[0][1].text + field[0][2].text
+                case 1:
+                    l = field[1][0].text + field[1][1].text + field[1][2].text
+                case 2:
+                    l = field[2][0].text + field[2][1].text + field[2][2].text
+                case 3:
+                    l = field[0][0].text + field[1][0].text + field[2][0].text
+                case 4:
+                    l = field[0][1].text + field[1][1].text + field[2][1].text
+                case 5:
+                    l = field[0][2].text + field[1][2].text + field[2][2].text
+                case 6:
+                    l = field[0][0].text + field[1][1].text + field[2][2].text
+                case 7:
+                    l = field[0][2].text + field[1][1].text + field[2][0].text
+                case _:
+                    l = None
+            if ((l == "XXX") or (l == "OOO")):
+                return True
+
+
+            
+
+
 
